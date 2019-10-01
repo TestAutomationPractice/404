805cf8c1006bd00db97b365ac27659f6da209c27
SHUBHAM KUMAR <sk9331657@gmail.com>
2019-10-01 12:19:44 +0530

    Save test1

--- api/specs/products/validateProductsApi.test.js
+++ api/specs/products/validateProductsApi.test.js
@@ -8,45 +8,45 @@ const request = require('supertest');
 const config = require('../../configuration/config.json');
 
 function makeid(length) {
-    var result           = '';
-    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var result = '';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     var charactersLength = characters.length;
-    for ( var i = 0; i < length; i++ ) {
-       result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    for (var i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
     }
     return result;
- }
+}
 
 describe('/products tests', () => {
 
 
-    test('Validate GET :: /movies', async() => {
-        
+    test('Validate GET :: /movies', async () => {
+
         const response = await request(config.baseUrl).get('movies')
             .set('Content-Type', 'application/json')
             .retry(3);
         await expect(response.statusCode).toBe(200);
-      
+
     });
 
 
-    test('Validate POST :: /movies', async() => {
+    test('Validate POST :: /movies', async () => {
 
 
         const response = await request(config.baseUrl).post('login')
-        .send({"username":"admin","password":"password"})
-        .retry(3);
-        
+            .send({ "username": "admin", "password": "password" })
+            .retry(3);
+
         const resp = await request(config.baseUrl).post('movies')
             .set('Content-Type', 'application/json')
-            .set('user',response.body.id)
-            .send({"movie":{"rating":2,"title":makeid(7),"description":makeid(10),"categories":["Comedy"],"image":"https://www.google.com","director":makeid(4),"reviews":[],"rented":false}})
+            .set('user', response.body.id)
+            .send({ "movie": { "rating": 2, "title": makeid(7), "description": makeid(10), "categories": ["Comedy"], "image": "https://www.google.com", "director": makeid(4), "reviews": [], "rented": false } })
             .retry(3);
         await expect(resp.statusCode).toBe(200);
-      
+
 
     });
 
 
-   
+
 });
